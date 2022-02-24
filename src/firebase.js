e2fe2f7da4cc55f7788d8fcbe8d8f53c38118f4d
Shahid Aaqeel <aaqeelzakeer0@gmail.com>
2022-02-24 18:48:03 +0530

    -Initial Commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDvZNdWkjtQ4FzsQQF_NMYIHidql4Vlgao",
+    authDomain: "whatsapp-clone-87938.firebaseapp.com",
+    projectId: "whatsapp-clone-87938",
+    storageBucket: "whatsapp-clone-87938.appspot.com",
+    messagingSenderId: "1070585429459",
+    appId: "1:1070585429459:web:d772f176d36049b0a18326",
+    measurementId: "G-GCG1YE9MFV"
+  };
